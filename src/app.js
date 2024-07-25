42c962a58cfbc576054b2e0f8521895b36602733
Kleber Coelho <klebercoelhogt@gmail.com>
2024-07-24 22:52:16 -0300

    #

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express, { json } from "express"; //Importando o express, json do Express
-
+import cors from "cors";
 import authRouter from "./routes/authRouter.js";
 
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
